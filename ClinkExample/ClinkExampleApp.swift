057f9f3a3e851d3a889b61015f1104c6a046917b
Mira Yang <miracolly330@gmail.com>
2024-09-08 03:04:03 -0400

    add gitignore and xcode project

---
+++ ClinkExample/ClinkExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinkExampleApp.swift
+//  ClinkExample
+//
+//  Created by Mira Yang on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ClinkExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
